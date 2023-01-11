f87c2fe9d69a9d09be4ca37a0d8d6052ec8ae16d
smile-namrata <namrata.jaiswal102441@gmail.com>
2023-01-11 21:12:57 +0530

    final Updation

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,6 +5,8 @@ import { useState } from 'react'
 const Home = () => {
 
   const [datas, setData] = useState([])
+  const [flag , setFlag] = useState(false)
+
   const getData = async () => {
     const { data } = await axios('https://randomuser.me/api/?results=50');
 
@@ -16,6 +18,7 @@ const Home = () => {
 
 
     setData(alldata.data.data);
+    setFlag(true);
 
   }
 
@@ -24,7 +27,7 @@ const Home = () => {
 
       <div >
         <h1>WELCOME</h1>
-        <button className='btn' onClick={() => { getData(); }}> fetch data </button>
+        <button className='btn' style={{  'cursor': flag ? 'not-allowed' : 'default'}} onClick={() => { flag ? alert('you already clickred') : getData(); }}> fetch data </button>
       </div>
       <div className='alldata'>
         {datas.length == 0 ? 'Click Button to fetch data' :
@@ -32,29 +35,29 @@ const Home = () => {
 
             <p className='data'>
               {<img style={{ borderStyle: "solid" }} src={ele.picture} />} <br />
-              Title:  {ele.title} <br />
-              Name: {ele.name} <br />
-              Gender: {ele.gender} <br />
-              Age: {ele.age} <br />
-              username : {ele.username} <br/>
-              Email : {ele.email} <br />
-              Password : {ele.password} <br/>
-              Location : {ele.location} <br/>
-              City: {ele.city}<br />
-              State:  {ele.state} <br />
-              Phone: {ele.phone} <br />
-              Cell : {ele.cell}  <br />
-              postcode:  {ele.postcode} <br />
-              Country:  {ele.country} <br/>
-              Longitude: {ele.longitude} <br/>
-              Latitude : {ele.latitude} <br/>
-              Description : {ele.description} <br/>
-              Timezone offset : {ele.offset} <br/>
-              Login uuid : {ele.uuid} <br/>
-              Login salt :{ele.salt} <br/>
-              Login md5 : {ele.md5} <br/>
-              Login sha1 : {ele.sha1} <br/>
-              Login sha256 : {ele.sha256} <br/>
+              <strong>Title: </strong> {ele.title} <br />
+              <strong>Name :</strong> {ele.name} <br />
+             <strong>Gender :</strong> {ele.gender} <br />
+             <strong>Age: </strong>{ele.age} <br />
+             <strong>username: </strong> {ele.username} <br/>
+             <strong>Email: </strong>   {ele.email} <br />
+             <strong>Password: </strong>  {ele.password} <br/>
+             <strong>Location: </strong> {ele.location} <br/>
+             <strong>City: </strong> {ele.city}<br />
+             <strong>State: </strong>   {ele.state} <br />
+             <strong>Phone: </strong>  {ele.phone} <br />
+             <strong>Cell: </strong> {ele.cell}  <br />
+             <strong>postcode: </strong>  {ele.postcode} <br />
+             <strong>Country: </strong>  {ele.country} <br/>
+             <strong>Longitude: </strong> {ele.longitude} <br/>
+             <strong>Latitude: </strong> {ele.latitude} <br/>
+             <strong>Description: </strong> {ele.description} <br/>
+             <strong>Timezone offset : </strong>{ele.offset} <br/>
+             <strong>Login uuid: </strong>  {ele.uuid} <br/>
+             <strong>Login salt : </strong>{ele.salt} <br/>
+             <strong> Login md5 : </strong> {ele.md5} <br/>
+             <strong>Login sha1 : </strong> {ele.sha1} <br/>
+             <strong>Login sha256 : </strong>  {ele.sha256} <br/>
          
             </p>
 
